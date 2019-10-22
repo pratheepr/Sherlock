78f22f8326916752044f1c5bfc563c3a1317da46
pratheepravysandirane <pratheepr>
2019-10-22 21:31:26 +0200

    Include DF definitions from tables & write output to postgres

--- sherlock.py
+++ sherlock.py
@@ -4,94 +4,81 @@ import numpy as np
 import pandas as pds
 from sqlalchemy import create_engine
 import db_connect as creds
-
+import io
 
 class create_dbConnection:
     def __init__(self, PGUSER, PGPASSWORD, PGHOST, PGDATABASE):
         self.alchemyEngine = create_engine(
             'postgresql+psycopg2://' + PGUSER + ':' + PGPASSWORD + ':@' + PGHOST + '/' + PGDATABASE, pool_recycle=3600)
-        self.dbConnection = self.alchemyEngine.connect()
 
+    def connect_to_db(self) -> object:
+        self.dbConnection = self.alchemyEngine.connect()
+        return self.dbConnection
 
 class create_DF:
     def __init__(self):
         self.temp = ''
 
-    def createDF(self, dbConnection, sql):
+    def createDF(self, dbConnection, sql) -> object:
         self.dataFrame = pds.read_sql(sql, dbConnection)
         return self.dataFrame
+
+
+def Write_DF_to_DB(postgre_def, df_capital_gains_losses):
+    df_capital_gains_losses.head(0).to_sql('public.stg_capt_gains_losses', postgre_def.alchemyEngine, if_exists='replace', index=False)  # truncates the table
+
+    conn = postgre_def.alchemyEngine.raw_connection()
+    cur = conn.cursor()
+    output = io.StringIO()
+    df_capital_gains_losses.to_csv(output, sep='\t', header=False, index=False)
+    output.seek(0)
+    contents = output.getvalue()
+    cur.copy_from(output, 'public.stg_capt_gains_losses', null="")  # null values become ''
+    conn.commit()
 
 
 def main():
     pds.set_option('display.expand_frame_repr', False)
 
-    postgre_conn = create_dbConnection(creds.PGUSER, creds.PGPASSWORD, creds.PGHOST, creds.PGDATABASE)
+    postgre_def = create_dbConnection(creds.PGUSER, creds.PGPASSWORD, creds.PGHOST, creds.PGDATABASE)
+    postgre_conn = postgre_def.connect_to_db()
 
     df_init = create_DF()
 
-    df_transactions = df_init.createDF(postgre_conn.dbConnection, "select * from public.inp_transaction")
+    df_transactions = df_init.createDF(postgre_conn, "select * from public.inp_transaction")
 
-   # print(df_transactions)
+    print(df_transactions)
 
     df_trans_sell = df_transactions.loc[df_transactions['annotation_value'] == 'Sell']
 
     df_trans_purchase = df_transactions.loc[df_transactions['annotation_value'] == 'Purchase']
 
-    df_capital_gains_losses = pds.DataFrame({'transaction': [''], 'charge_type': [''], 'date_trade': [''],
-                                                   'date_value': [''],
-                                                   'original_currency': [''],  'exchange_rate': [''] ,'pieces': [''],
-                                                   'sell_price': [''], 'purchase_price': [''] ,'proceeds_in_euro': [''],
-                                                   'fees_charges_in_euro': [''], 'acquisition_costs_in_euro': [''],
-                                                   'capital_gain_loss_in_euro': [''],
-                                                   'fx_component_in_gain_loss_euro': [''], 'total_gain_loss_eurp': [''],
-                                                   'orig_pieces':[''],  'final_pieces': ['']
-                                             })
+    df_capital_gains_losses = df_init.createDF(postgre_conn, "select * from public.stg_capt_gains_losses where 0=1")
 
-    row_cap_gain_loss = pds.DataFrame({'transaction': [''], 'charge_type': [''], 'date_trade': [''] ,
-                                                   'date_value': [''] ,
-                                                   'original_currency': [''] ,  'exchange_rate': [''] ,'pieces': [''] ,
-                                                   'sell_price': [''], 'purchase_price': [''] ,'proceeds_in_euro': [''],
-                                                   'fees_charges_in_euro': [''], 'acquisition_costs_in_euro': [''],
-                                                   'capital_gain_loss_in_euro': [''],
-                                                   'fx_component_in_gain_loss_euro': [''], 'total_gain_loss_eurp': [''],
-                                                   'orig_pieces':[''],  'final_pieces': ['']
-
-                                       })
-
-   # row_cap_gain_loss = pds.DataFrame()
+    row_cap_gain_loss = df_init.createDF(postgre_conn, "select * from public.stg_capt_gains_losses where 0=1")
 
     sell_count = 0
     purchase_count = 0
 
     for sell_index, sell_row in df_trans_sell.iterrows():
+
         sell_count = sell_row['no_of_items']
         purchase_count = 0
+
         for purch_index, purchase_row in df_trans_purchase.iterrows():
-            # if (sell_row['no_of_items'] - ( purchase_row['no_of_items'] + purchase_count)  > 0 ) :
-            #     purchase_count = purchase_row['no_of_items'] + purchase_count
-            #     df_capital_gains_losses = df_capital_gains_losses.append(df_trans_purchase)
-            # else:
-            #     purchase_count = purchase_count + (sell_count - purchase_row['no_of_items'])
-            #     df_trans_purchase.at[purch_index,'no_of_items'] = ( purchase_count + (sell_count - purchase_row['no_of_items']) )
+
+            row_cap_gain_loss = df_init.createDF(postgre_conn,
+                                                 "select * from public.stg_capt_gains_losses where 0=1")
 
             # if sell instrument is greater than purchase
             if ((sell_count - purchase_row['no_of_items']) >= 0) and (purchase_row['no_of_items'] > 0):
-                row_cap_gain_loss = pds.DataFrame({'transaction': [''], 'charge_type': [''], 'date_trade': [''] ,
-                                                   'date_value': [''] ,
-                                                   'original_currency': [''] ,  'exchange_rate': [''] ,'pieces': [''] ,
-                                                   'sell_price': [''], 'purchase_price': [''] ,'proceeds_in_euro': [''],
-                                                   'fees_charges_in_euro': [''], 'acquisition_costs_in_euro': [''],
-                                                   'capital_gain_loss_in_euro': [''],
-                                                   'fx_component_in_gain_loss_euro': [''], 'total_gain_loss_eurp': [''],
-                                                   'orig_pieces':[''],  'final_pieces':['']
-                                                   })
 
                 purchase_count = purchase_row['no_of_items'] + purchase_count
 
                 row_cap_gain_loss.at[0, 'transaction'] = 'Sell'
                 row_cap_gain_loss.at[0, 'charge_type'] = 'Taxable'
                 row_cap_gain_loss.at[0, 'date_trade'] = sell_row['date_trade']
-                row_cap_gain_loss.at[0, 'date_value'] = sell_row['date_value']
+                row_cap_gain_loss.at[0, 'value_date'] = sell_row['date_value']
                 row_cap_gain_loss.at[0, 'original_currency'] = 'EU'
                 row_cap_gain_loss.at[0, 'exchange_rate'] = 1
                 row_cap_gain_loss.at[0, 'pieces'] = purchase_row['no_of_items']
@@ -105,39 +92,31 @@ def main():
                 row_cap_gain_loss.at[0, 'capital_gain_loss_in_euro'] = (row_cap_gain_loss.loc[0, 'proceeds_in_euro']) - (row_cap_gain_loss.loc[0, 'acquisition_costs_in_euro'] + row_cap_gain_loss.loc[0, 'fees_charges_in_euro'])
                 row_cap_gain_loss.at[0, 'fx_component_in_gain_loss_euro'] = 0
                 row_cap_gain_loss.at[0, 'total_gain_loss_eurp'] = row_cap_gain_loss.loc[0, 'capital_gain_loss_in_euro']
+                row_cap_gain_loss.at[0, 'sell_transaction_id'] = sell_row['transaction_id']
+                row_cap_gain_loss.at[0, 'purchase_transaction_id'] = purchase_row['transaction_id']
 
                 print(row_cap_gain_loss)
 
-               # df_capital_gains_losses =df_capital_gains_losses.append(row_cap_gain_loss, ignore_index= True)
-
                 sell_count = sell_count - purchase_row['no_of_items']
-                orig_pieces =  purchase_row['no_of_items']
+                orig_pieces = purchase_row['no_of_items']
                 row_cap_gain_loss.at[0, 'orig_pieces'] = orig_pieces
 
                 final_pieces = 0
                 df_trans_purchase.at[purch_index, 'no_of_items'] = 0
                 row_cap_gain_loss.at[0, 'final_pieces'] = final_pieces
 
-                df_capital_gains_losses =df_capital_gains_losses.append(row_cap_gain_loss, ignore_index= True)
+                df_capital_gains_losses = df_capital_gains_losses.append(row_cap_gain_loss, ignore_index= True)
 
 
                 continue
 
             if ((sell_count - purchase_row['no_of_items']) < 0)  and (purchase_row['no_of_items'] > 0):
-                row_cap_gain_loss = pds.DataFrame({'transaction': [''], 'charge_type': [''], 'date_trade': [''],
-                                                   'date_value': [''],
-                                                   'original_currency': [''], 'exchange_rate': [''], 'pieces': [''],
-                                                   'sell_price': [''], 'purchase_price': [''], 'proceeds_in_euro': [''],
-                                                   'fees_charges_in_euro': [''], 'acquisition_costs_in_euro': [''],
-                                                   'capital_gain_loss_in_euro': [''],
-                                                   'fx_component_in_gain_loss_euro': [''], 'total_gain_loss_eurp': ['']
-                                                   })
 
                 purchase_count =  sell_count # purchase_count +
                 row_cap_gain_loss.at[0, 'transaction'] = 'Sell'
                 row_cap_gain_loss.at[0, 'charge_type'] = 'Taxable'
                 row_cap_gain_loss.at[0, 'date_trade'] = sell_row['date_trade']
-                row_cap_gain_loss.at[0, 'date_value'] = sell_row['date_value']
+                row_cap_gain_loss.at[0, 'value_date'] = sell_row['date_value']
                 row_cap_gain_loss.at[0, 'original_currency'] = 'EU'
                 row_cap_gain_loss.at[0, 'exchange_rate'] = 1
                 row_cap_gain_loss.at[0, 'pieces'] = sell_count
@@ -153,9 +132,6 @@ def main():
                 row_cap_gain_loss.at[0, 'total_gain_loss_eurp'] = row_cap_gain_loss.loc[0, 'capital_gain_loss_in_euro']
 
 
-             #   df_capital_gains_losses =df_capital_gains_losses.append(row_cap_gain_loss, ignore_index= True)
-
-
                 df_trans_purchase.at[purch_index, 'no_of_items'] = purchase_row['no_of_items'] - sell_count
 
                 orig_pieces = purchase_row['no_of_items']
@@ -164,25 +140,18 @@ def main():
                 row_cap_gain_loss.at[0, 'orig_pieces'] = orig_pieces
                 row_cap_gain_loss.at[0, 'final_pieces'] = final_pieces
 
+                row_cap_gain_loss.at[0, 'sell_transaction_id'] = sell_row['transaction_id']
+                row_cap_gain_loss.at[0, 'purchase_transaction_id'] = purchase_row['transaction_id']
+
                 sell_count = 0
 
-                df_capital_gains_losses =df_capital_gains_losses.append(row_cap_gain_loss, ignore_index= True)
+                df_capital_gains_losses = df_capital_gains_losses.append(row_cap_gain_loss, ignore_index= True)
 
                 continue
 
             if sell_count == 0:
                 break
 
-
-            #print(" ***********************PURCHASE TRANSACTIONS*********************** ")
-            #print(df_trans_purchase)
-
-
-    print("PROFIT LOSS STATEMENT  ")
-    print(df_capital_gains_losses)
-
-
-    # print(df_trans_sell)
-
+    Write_DF_to_DB(postgre_def, df_capital_gains_losses)
 
 main()
